3dac722bb6dbe1053aaa233c357a8ccc26c14f9f
hp <hp@om>
2020-09-05 09:11:30 +0530

    Commit by Bhau

--- Git_Demo/src/github/Demo.java
+++ Git_Demo/src/github/Demo.java
@@ -2,6 +2,10 @@ package github;
 
 public class Demo 
 {
+	public void sum()
+	{
+		System.out.println("Sum Method");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Main Method");
