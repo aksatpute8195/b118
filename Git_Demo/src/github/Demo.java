65c1c9e34462264bdcbcae1851525b83e76d940b
hp <hp@om>
2020-09-07 14:38:27 +0530

    this is the message

--- Git_Demo/src/github/Demo.java
+++ Git_Demo/src/github/Demo.java
@@ -6,8 +6,8 @@ public class Demo
 	{
 		System.out.println("Sum Method");
 	}
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello Main Method");
 	}
 
